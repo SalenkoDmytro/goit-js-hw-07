1edc3402228f058ccfff39a4755b8d6bf81c1e02
SalenkoDmytro <dmytrosalenko1996@gmail.com>
2022-07-25 09:23:48 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems));
+
+galleryRef.addEventListener('click', onGalleryRefClick);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        ` <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    )
+    .join('');
+}
+
+function onGalleryRefClick(event) {
+  console.log(event);
+}
